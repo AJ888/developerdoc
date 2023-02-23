9ade6be0409cc9888b9b8931f228f3203270a52f
ammar <aj@ammars-MBP-2.lan>
2023-02-22 19:09:31 -0600

    create an API endpoint so that it can give our React app data, change that data, or do some other operation only a server can do.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//using api end-point to exchange data between back-end and front-end
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
